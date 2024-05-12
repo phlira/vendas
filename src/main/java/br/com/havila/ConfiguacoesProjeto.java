8a539739841956169f24bc93c82ca808dc1c0696
Pedro Lira <pedrohsoares.lira@gmail.com>
2024-05-11 23:23:20 -0300

    Primeiro modulo finalizado

--- src/main/java/br/com/havila/ConfiguacoesProjeto.java
+++ src/main/java/br/com/havila/ConfiguacoesProjeto.java
@@ -1,13 +1,15 @@
 package br.com.havila;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class ConfiguacoesProjeto {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de vendas Havilá Consultoria";
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO EM AMBIENTE DE DESENVOLVIMENTO");
+        };
     }
 }
